239c78710e2aa98fad06acbd32fee8aa968c4a5f
Joonas (Helioy) <joonas@helsinginlipputehdas.fi>
2020-07-29 16:16:34 +0300

    heli 20-07-29 16:16

--- WikiApi/Program.cs
+++ WikiApi/Program.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace WikiApi
 {
@@ -12,15 +14,33 @@ namespace WikiApi
         static void Main(string[] args)
         {
             Console.SetWindowSize(100, 20);
+                        
+            Console.Write("Wikipedia title/link: ");
+            string pageTitle = Console.ReadLine();
+            if (pageTitle.Contains("/wiki/"))
+            {
+                pageTitle = pageTitle.Split(new string[] { "/wiki/" }, StringSplitOptions.None).Last();
+            }
+             
+            string searchURL = "https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=" + pageTitle;
+            Console.WriteLine("Reading " + searchURL + "/ ...");
 
-            Console.Write("Article name: ");
-            string articleName = Console.ReadLine();
-            string queryURL = "https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=" + articleName;
-            Console.WriteLine("Reading " + queryURL + "/ ...");
+            Task<string> taskResult = GetHttpContent(searchURL);
+            string content = taskResult.Result;
 
-            Task<string> taskResult = GetHttpContent(queryURL);
-            string content = taskResult.Result;            
-            Console.WriteLine(content);
+            JObject searchData = JObject.Parse(content);
+            int totalHits = Int32.Parse(searchData["query"]["searchinfo"]["totalhits"].ToString());
+            if (totalHits > 0)
+            {
+                string pageID = searchData["query"]["search"][0]["pageid"].ToString();
+                Console.WriteLine("pageID=" + pageID);
+                string queryURL = "https://en.wikipedia.org/w/api.php?action=query&prop=extracts&exintro&pageids=" + pageID + "&format=json";
+                string queryContent = GetHttpContent(queryURL).Result;
+                JObject queryData = JObject.Parse(queryContent);
+                string article = queryData["query"]["pages"][pageID]["extract"].ToString();
+
+                Console.WriteLine(article);
+            }
         }
 
 
